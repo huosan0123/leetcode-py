af5ab2bff29604ce511a92ab3589907caaf0c733
huosan0123 <lc_286@126.com>
2017-09-04 11:56:56 +0800

    007

---
+++ 007.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:             #分情况讨论
+            ch = str(-x)      #负数变成正数在str
+            rch = ch[::-1]     #切片神迹
+            n = 0 - int(rch)
+        else:
+            ch = str(x)
+            rch = ch[::-1]
+            n = int(rch)
+        if n < -2**31 or n > 2**31-1: #32bit sighed integer的上下限
+            return 0
+        else:
+            return n
