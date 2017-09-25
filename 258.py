e6dd4d319ef176a820af7e2619ce194177730991
huosan0123 <lc_286@126.com>
2017-09-25 16:41:36 +0800

    258

---
+++ 258.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0: return 0
+        return num - 9 * ((num-1)/9)        #this is a math problem.
