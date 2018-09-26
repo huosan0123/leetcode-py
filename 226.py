fd35d852bc57f6f11569664f10b7bd7cc83f9fe3
huosan0123 <lc_286@126.com>
2018-09-26 22:49:25 +0800

    must know non-recursive solution

--- 226.py
+++ 226.py
@@ -18,3 +18,19 @@ class Solution(object):
             self.invertTree(root.left)
             self.invertTree(root.right)
         return root
+
+TreeNode* invertTree(TreeNode* root) {
+    std::stack<TreeNode*> stk;
+    stk.push(root);
+    
+    while (!stk.empty()) {
+        TreeNode* p = stk.top();
+        stk.pop();
+        if (p) {
+            stk.push(p->left);
+            stk.push(p->right);
+            std::swap(p->left, p->right);
+        }
+    }
+    return root;
+}
