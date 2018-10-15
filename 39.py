00cb7190c2560a1206b53c1f504bed10cfec46ab
huosan0123 <lc_286@126.com>
2018-10-16 00:35:39 +0800

    copy, must learn

--- 39.py
+++ 39.py
@@ -16,3 +16,24 @@ class Solution(object):
             res.append(path)
         for i in range(index, len(nums)):
             self.dfs(nums, target-nums[i], i, path+[nums[i]], res)
+
+# must konw why this answer returns unique solution
+
+# using inner function to avoid parameter sending, a little bit faster.
+def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        def dfs(t, i, path, res):
+            if t < 0:
+                return
+            if t == 0:
+                res.append(path)
+            for k in range(i, len(candidates)):
+                dfs(t-candidates[k], k, path+[candidates[k]], res)
+        
+        dfs(target, 0, [], res)
+        return res
