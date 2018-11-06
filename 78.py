fbe7fc563bdb6c46742521dfcd9dc68493d6fe07
huosan0123 <lc_286@126.com>
2018-11-06 19:49:51 +0800

    copy

--- 78.py
+++ 78.py
@@ -13,3 +13,11 @@ class Solution(object):
         
         dfs(0, [], ans)
         return ans
+    
+    # iteratvie, spend more time
+    def answers(self, nums):
+        ans = [[]]
+
+            for n in nums:
+                ans += [s+[n] for s in ans]
+            return ans
