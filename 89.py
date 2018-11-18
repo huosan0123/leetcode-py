5286753b1c63e339e45528eb972e430cc91eb0e8
huosan0123 <lc_286@126.com>
2018-11-18 16:05:40 +0800

    cp

--- 89.py
+++ 89.py
@@ -9,3 +9,24 @@ class Solution(object):
         for i in range(n):
             ans.extend([a + 2**i for a in ans[::-1]])
         return ans
+
+
+class Solution {
+    void utils(bitset<32>& bits, vector<int>& result, int k){
+        if (k==0) {
+            result.push_back(bits.to_ulong());
+        }
+        else {
+            utils(bits, result, k-1);
+            bits.flip(k-1);
+            utils(bits, result, k-1);
+        }
+    }
+public:
+    vector<int> grayCode(int n) {
+        bitset<32> bits;
+        vector<int> result;
+        utils(bits, result, n);
+        return result;
+    }
+};
