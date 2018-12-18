780ca0debda0a6caf8f3ea3b74ba59bd21920614
huosan0123 <lc_286@126.com>
2018-12-19 00:01:03 +0800

    self

--- 113.py
+++ 113.py
@@ -29,3 +29,28 @@ class Solution(object):
         dfs(root, [], sum)
         return ans
         
+    def pathSum(self, root, sum):
+        """
+        改动一小部分，就超过100%，简直666啊
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        ans = []
+        
+        def dfs(node, path, c):
+            if not node.left and not node.right:
+                if (c - node.val) == 0:
+                    ans.append(path+[node.val])   # 这里符合路径，直接复制数组
+                    return
+            c -= node.val
+            path.append(node.val)     # 因为复制数组耗时，所以更改为只在必要的时候复制。
+            if node.left:
+                dfs(node.left, path, c)
+            if node.right:
+                dfs(node.right, path, c)
+            path.pop()                 # 退站的时候，取出放入的节点
+        dfs(root, [], sum)
+        return ans
