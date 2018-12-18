201a86c019448280deed446fa30809a3c2fca4b5
huosan0123 <lc_286@126.com>
2018-12-18 23:44:35 +0800

    half cp

--- 199.py
+++ 199.py
@@ -19,3 +19,26 @@ class Solution(object):
             self.post(node.right, depth+1, ans)
         if node.left:
             self.post(node.left, depth+1, ans)
+
+    def rightSideView(self, root):
+        """
+        这种解法符合我的思想，当时我不知道如何实现。
+        看了别人代码和知道了deque之后，会的
+        """
+        if root is None:
+            return []
+        
+        from collections import deque
+        de = deque()
+        de.append(root)
+        ans = []
+        while de:
+            ans.append(de[-1].val)
+            for i in range(len(de)):
+                p = de.popleft()
+                if p.left:
+                    de.append(p.left)
+                if p.right:
+                    de.append(p.right)
+        return ans
+        
