4c4f9e5cbfa4d6b76dea88392ab78941b87c49a7
huosan0123 <lc_286@126.com>
2018-12-25 19:46:57 +0800

    self,

--- 173.py
+++ 173.py
@@ -26,3 +26,34 @@ class BSTIterator(object):
         p = self.st.pop()
         self.root = p.right
         return p.val
+
+#  this is another similiar solution, also good
+class BSTIterator(object):
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = []
+        curr = root
+        while curr is not None:
+            self.stack.append(curr)
+            curr = curr.left
+            
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return bool(self.stack)
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        curr = self.stack.pop()
+        val = curr.val
+        if curr.right:
+            curr = curr.right
+            while curr:
+                self.stack.append(curr)
+                curr = curr.left
+        return val
