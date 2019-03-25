ab902cf85b6e45bd3dc4756374baa89d46dd2e99
huosan0123 <lc_286@126.com>
2019-03-25 11:29:07 +0800

    half,key

--- 110.py
+++ 110.py
@@ -28,3 +28,21 @@ class Solution(object):
             left_h = self.getHeight(root.left)
             right_h = self.getHeight(root.right)
             return max(left_h, right_h) + 1
+
+链接：https://www.nowcoder.com/questionTerminal/8b3b95850edb4115918ecebdf1b4d222
+来源：牛客网
+# 上面的解法同一个节点遍历太多次，这个解法很棒，剪枝
+public class Solution {
+    public boolean IsBalanced_Solution(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+     
+    private int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = getDepth(root.left);
+        if (left == -1) return -1;
+        int right = getDepth(root.right);
+        if (right == -1) return -1;
+        return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+    }
+}
